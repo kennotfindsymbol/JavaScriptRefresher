ca72438e0971258fe683e54310d07d613082e900
kennotfindsymbol <kennotfindsymbol@gmail.com>
2024-11-02 14:24:21 +0800

    Add object examples

--- play.js
+++ play.js
@@ -58,3 +58,19 @@ console.log(summarizeUserArrow(myName, myAge, likesCoding));
 console.log(addWithReturn(1, 2));
 console.log(addWithoutReturn(1, 2));
 console.log(addNoArgument());
+
+// an object
+const person = {
+  name: "Ken",
+  age: 21,
+  greetNormal() {
+    console.log("Hi, I am " + this.name + ". I am " + this.age + " years old.");
+  },
+  // gives "Hi I am underfined" because "this" refers to global scope
+  greetArrow: () => {
+    console.log("Hi, I am " + this.name + ". I am " + this.age + " years old.");
+  },
+};
+
+person.greetNormal();
+person.greetArrow();
