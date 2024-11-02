10f9fc2af1606022fb7185cf57357dfa2ad53c7d
kennotfindsymbol <kennotfindsymbol@gmail.com>
2024-11-02 15:41:07 +0800

    Create async.js

---
+++ async.js
@@ -0,0 +1,25 @@
+// run "node async.js" in terminal
+
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, 1500);
+  });
+  return promise;
+};
+
+setTimeout(() => {
+  console.log("2000 Timer is done!");
+  fetchData()
+    .then((text) => {
+      console.log(text);
+      return fetchData();
+    })
+    .then((text2) => {
+      console.log(text2);
+    });
+}, 2000);
+
+// synchronous code will always be executed first
+console.log("I am synchronous code");
